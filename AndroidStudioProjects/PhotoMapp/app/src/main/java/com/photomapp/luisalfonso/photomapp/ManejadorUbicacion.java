cbfd040bb5e20c96d03aed9147af57a9b0395d4a
Luis Alfonso <luixlive@gmail.com>
2016-03-07 02:06:02 -0600

    ManejadorUbicacion Se agrega una clase que maneja las ubicaciones (obtener, actualizar) de forma responsiva.

---
+++ AndroidStudioProjects/PhotoMapp/app/src/main/java/com/photomapp/luisalfonso/photomapp/ManejadorUbicacion.java
@@ -0,0 +1,136 @@
+package com.photomapp.luisalfonso.photomapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+import com.photomapp.luisalfonso.photomapp.Activities.ActivityPrincipal;
+
+/**
+ * Clase ManejadorUbicacion: Apoya con buenas practicas para la obtencion de la ubicacion segun:
+ * http://developer.android.com/intl/es/guide/topics/location/strategies.html
+ */
+public class ManejadorUbicacion {
+
+    //Macros para actualizacion de ubicacion (cada 30 segundos o cada que el usuario se mueva 25 metros)
+    private static final int TIEMPO_ACTUALIZACION = 1000*30;
+    private static final int DISTANCIA_ACTUALIZACION = 25;
+    private static final int DOS_MINUTOS = 1000*60*2;
+
+    //Variables de apoyo
+    private LocationManager manejador;
+    private Activity activity_padre;
+    private Location ultima_ubicacion = null;
+    private LocationListener cambio_ubicacion_listener;
+
+    /**
+     * ManejadorUbicacion; Constructor, inicia las variables de apoyo
+     * @param activity Activity padre para el acceso a los servicios de ubicacion
+     */
+    public ManejadorUbicacion(Activity activity) {
+        activity_padre = activity;
+        manejador = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        cambio_ubicacion_listener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                //Si se detecta una nueva ubicacion se checa si es mejor y de ser asi actualiza la ultima ubicacion
+                if (esMejorUbicacion(location, ultima_ubicacion))
+                    ultima_ubicacion = location;
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        //Obtenemos la ultima ubicacion conocida por el servicio telefonico en caso de que se tome una foto antes
+        //de acceder al GPS
+        if (ActivityCompat.checkSelfPermission(activity_padre, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity_padre,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity_padre, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, ActivityPrincipal.PERMISO_ACCESO_UBICACION);
+        }
+        ultima_ubicacion = manejador.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
+
+    /**
+     * comenzarActualizacionUbicacion: Comienza a escuchar los eventos de cambio de ubicacion (gasta bateria,
+     * por lo que es recomendable usar detenerActualizaiconUbicacion en cuanto ya no se necesite.
+     */
+    public void comenzarActualizacionUbicacion() {
+        if (ActivityCompat.checkSelfPermission(activity_padre, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity_padre,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity_padre, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, ActivityPrincipal.PERMISO_ACCESO_UBICACION);
+        }
+        manejador.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIEMPO_ACTUALIZACION, DISTANCIA_ACTUALIZACION,
+                cambio_ubicacion_listener);
+    }
+
+    /**
+     * detenerActualizacionUbicacion: Detiene los eventos de ubicacion.
+     */
+    public void detenerActualizacionUbicacion() {
+        if (ActivityCompat.checkSelfPermission(activity_padre, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity_padre,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity_padre, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, ActivityPrincipal.PERMISO_ACCESO_UBICACION);
+        }
+        manejador.removeUpdates(cambio_ubicacion_listener);
+    }
+
+    /**
+     * obtenerUbicacion: Regresa la ubicacion mas reciente obtenida.
+     * @return Location con la ultima ubicacion conocida.
+     */
+    public Location obtenerUbicacion(){
+        return ultima_ubicacion;
+    }
+
+    /**
+     * esMejorUbicacion: Algoritmo simple para determinar si es conveniente o no actualizar la ultima ubicacion
+     * de acuerdo a las caracteristicas de la nueva y vieja ubicacion.
+     * @param ubicacion_nueva Location nueva
+     * @param actual_mejor_ubicacion Location que se tenia como la mejor ubicacion
+     * @return true si es conveniente acutualizar a la nueva, false de toro modo
+     */
+    protected boolean esMejorUbicacion(Location ubicacion_nueva, Location actual_mejor_ubicacion) {
+        if (actual_mejor_ubicacion == null) {
+            return true;
+        }
+
+        //Primero checamos que tanta diferencia de tiempo hay, si es mucho mas nueva si conviene cambiar
+        long diferencia_tiempo = ubicacion_nueva.getTime() - actual_mejor_ubicacion.getTime();
+        boolean es_mucho_mas_nueva = diferencia_tiempo > DOS_MINUTOS;
+        boolean es_nueva = diferencia_tiempo > 0;
+        if (es_mucho_mas_nueva) {
+            return true;
+        }
+
+        //Ahora la precision, si es mas precisa, combiene cambiar
+        int diferencia_presicion = (int) (ubicacion_nueva.getAccuracy() - actual_mejor_ubicacion.getAccuracy());
+        boolean menos_precisa = diferencia_presicion > 0;
+        boolean mas_precisa = diferencia_presicion < 0;
+        //Si es igual de precisa pero mas nueva tambien combiene cambiar
+        if (mas_precisa) {
+            return true;
+        } else if (es_nueva && !menos_precisa) {
+            return true;
+        }
+        return false;
+    }
+
+}
